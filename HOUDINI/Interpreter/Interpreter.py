508d96129749dee5ba5799dd02e16c6390f6474f
Mrunmay Shelar <mrunmayshelar@gmail.com>
2022-12-09 18:31:01 +0530

    'fixeed'

--- HOUDINI/Interpreter/Interpreter.py
+++ HOUDINI/Interpreter/Interpreter.py
@@ -481,3 +481,180 @@ def _get_unknown_fns_definitions(unkSortMap, is_graph=False):
         #unk_fns_interpreter_def_list.append(uf)
 
     return unk_fns_interpreter_def_list
+def map2d(fn, iterable):
+    if type(iterable) == tuple:
+        iterable = iterable[1]
+
+    if isinstance(iterable, torch.autograd.variable.Variable):
+        iterable = split(iterable)
+        iterable = [split(i) for i in iterable]
+
+    result = [[fn(j) for j in i] for i in iterable]
+    return result
+
+
+def map_g(fn):
+    def ret(iterable):
+        if type(iterable) == tuple:
+            iterable = iterable[1]
+
+        if isinstance(iterable, torch.autograd.variable.Variable):
+            iterable = split(iterable)
+            iterable = [split(i) for i in iterable]
+
+        result = [[fn(j) for j in i] for i in iterable]
+        return result
+    return ret
+
+
+def flatten_2d_list(iterable):
+    assert (type(iterable) == list)
+    assert (iterable.__len__() > 0)
+    assert (type(iterable[0]) == list)
+    return [item for innerlist in iterable for item in innerlist]
+
+
+def cat(a, b):
+    if type(a) == tuple:
+        a = a[1]
+    if type(b) == tuple:
+        b = b[1]
+
+    return torch.cat((a, b), dim=1)
+
+
+def map(fn, iterable):
+    if isinstance(iterable, torch.autograd.variable.Variable):
+        iterable = split(iterable)
+    if iterable.__len__() > 0 and type(iterable[0]) == tuple:
+        iterable = [i[1] for i in iterable]
+    # using list() to force the map to be evaluated, otherwise it's lazily evaluated
+    return list(map(fn, iterable))
+
+
+def map_list(fn):
+    def ret(iterable):
+        if isinstance(iterable, torch.autograd.variable.Variable):
+            iterable = split(iterable)
+        if iterable.__len__() > 0 and type(iterable[0]) == tuple:
+            iterable = [i[1] for i in iterable]
+        # using list() to force the map to be evaluated, otherwise it's lazily evaluated
+        return list(map(fn, iterable))
+
+    return ret
+
+
+def conv_list(fn):
+    def ret(iterable):
+        if isinstance(iterable, torch.autograd.variable.Variable):
+            iterable = split(iterable)
+        if iterable.__len__() > 0 and type(iterable[0]) == tuple:
+            iterable = [i[1] for i in iterable]
+
+        if iterable.__len__() == 0:
+            return []
+
+        if type(iterable[0]) != torch.autograd.variable.Variable:
+            raise NotHandledException
+
+        # zero-pad start and end
+        zero = torch.zeros_like(iterable[0])
+        iterable.insert(0, zero)
+        iterable.append(zero)
+        result = []
+        for idx in range(1, iterable.__len__() - 1):
+            c_arr = [iterable[idx - 1], iterable[idx], iterable[idx + 1]]
+            result.append(fn(c_arr))
+        return result
+
+    return ret
+
+
+def conv_graph(fn):
+    return lambda x: fn(x)
+
+
+def reduce_graph(fn):
+    raise NotImplementedError
+
+
+def reduce_list(fn, init=None):
+    def ret(iterable):
+        if isinstance(iterable, torch.autograd.variable.Variable):
+            iterable = split(iterable)
+        if iterable.__len__() > 0 and type(iterable[0]) == tuple:
+            iterable = [i[1] for i in iterable]
+        return reduce(fn, iterable) if init is None else reduce(fn, iterable, init)
+
+    return ret
+
+
+def repeat(num_times_to_repeat, fn):
+    def ret(x):
+        for i in range(num_times_to_repeat):
+            x = fn(x)
+        return x
+
+    return ret
+
+
+def compose(g, f):
+    return lambda x: g(f(x))
+
+
+def reduce(fn, iterable, initializer=None):
+    if isinstance(iterable, torch.autograd.variable.Variable):
+        iterable = split(iterable)
+    if iterable.__len__() > 0 and type(iterable[0]) == tuple:
+        iterable = [i[1] for i in iterable]
+    return reduce(fn, iterable) if initializer is None else reduce(fn, iterable, initializer)
+
+
+def get_zeros(dim):
+    """
+    Returns zeros of shape [var_x.shape[0], dim]
+    Atm, it's used for initializing hidden state
+    :return:
+    """
+    """
+    if type(var_x) == tuple:
+        var_x = var_x[1]
+    """
+
+    zeros = torch.zeros(1, dim)
+    if torch.cuda.is_available():
+        zeros = zeros.cuda()
+    return torch.autograd.variable.Variable(zeros)
+
+
+def multiply_by_range09():
+    # zeros = torch.zeros(var_x.data.shape[0], dim)
+    range_np = np.array([0., 1., 2., 3., 4., 5., 6., 7., 8., 9.], dtype=np.float32).reshape((10, 1))
+    range_torch = torch.from_numpy(range_np)
+    if torch.cuda.is_available():
+        range_torch = range_torch.cuda()
+    range_var = torch.autograd.variable.Variable(range_torch)
+
+    def pp_multiply_by_range09(inputs):
+        return torch.matmul(inputs, range_var)
+
+    return pp_multiply_by_range09
+
+
+def argmax(x):
+    # x = list(x)
+    if type(x) == tuple:
+        x = x[1]
+    values, indices = torch.max(x, 1, keepdim=True)
+    # values, indices = torch.max(input=x, dim=1, keepdim=True)
+    # return indices
+    indices = indices.float()
+    return indices
+
+
+def add(x, y):
+    if type(x) == tuple:
+        x = x[1]
+    if type(y) == tuple:
+        y = y[1]
+    return x + y
